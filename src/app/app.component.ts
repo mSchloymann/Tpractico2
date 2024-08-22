d8f43145a6652eeaf6897ae892689d57d946928c
Liesel <marceloschloymann@gmail.com>
2024-08-22 17:31:24 -0300

    Tpractico2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from "./home/home.component";
+import { CatalogoComponent } from "./catalogo/catalogo.component";
+import { RegistroComponent } from "./registro/registro.component";
+import { LoginComponent } from "./login/login.component";
+import { DetalleComponent } from "./detalle/detalle.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, CatalogoComponent, LoginComponent, DetalleComponent, RegistroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/registro/registro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro',
+  standalone: true,
+  imports: [],
+  template: './registro.component.html',
+  styleUrl: './registro.component.css'
+})
+export class RegistroComponent {
+
+}
